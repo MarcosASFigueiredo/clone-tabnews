ac694ceaabaecafdee18c5dff3f04f0684665662
MarcosASFigueiredo <103333881+MarcosASFigueiredo@users.noreply.github.com>
2024-02-26 03:29:37 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oie minha ShyLittleFroga, te amo muito. Se você também me ama muito dá um soriso lindo!</h1>
+    return <h1>Oie minha ShyLittleFroga, te amo muito. Se você também me ama muito dá um soriso lindo! ❤️</h1>
 }
 
 export default Home;
