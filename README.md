0d941c18266a1cf9a5a2eed2ab507c89ad11389b
MarcosASFigueiredo <103333881+MarcosASFigueiredo@users.noreply.github.com>
2024-03-01 03:19:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Projeto para aprender do 0  implementação de uma aplicação real, baseado no https://www.tabnews.com.br
+
+Projeto para aprender do 0 implementação de uma aplicação real, baseado no https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Oie minha ShyLittleFroga, te amo muito. Se você também me ama muito dá um soriso lindo! ❤️</h1>
+  return (
+    <h1>
+      Oie minha ShyLittleFroga, te amo muito. Se você também me ama muito dá um
+      soriso lindo! ❤️
+    </h1>
+  );
 }
 
 export default Home;
