53c1de89fd019bc3cbfd134979c994acfaa7602b
Marcos Figueiredo <103333881+MarcosASFigueiredo@users.noreply.github.com>
2025-01-19 01:26:56 +0000

    fix: handle `client` as `undefined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log("\n erro dento do catch do database.js:");
     console.log(error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
