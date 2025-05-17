c1069f22722797feec531d047465379cbf17a759
Marcos Figueiredo <103333881+MarcosASFigueiredo@users.noreply.github.com>
2025-05-17 00:44:25 +0000

    refactor: change in progress page text

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Oie minha ShyLittleFroga, te amo muito. Se você também me ama muito dá um
-      soriso lindo! ❤️
-    </h1>
-  );
+  return <h1>In progress!</h1>;
 }
 
 export default Home;
